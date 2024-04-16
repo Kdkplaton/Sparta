ceceaf511356001981cf4130ec3a9cb3ed843618
Kdkplaton <kdkplaton@naver.com>
2024-04-16 21:34:06 +0900

    문제 1번 해결

---
+++ Algorithm_CodeKata/No_01.js
@@ -0,0 +1,19 @@
+// 문제 1 - 두 수의 차
+
+// Node.js 과정이 Javascript을 사용하니
+// javascript로 문제 풀이를 시도하자!
+
+// raw code
+function solution(num1, num2) {
+    var answer = 0;
+    return answer;
+}
+
+// 해결 코드
+function solution_solved(num1, num2) {
+    var sum = num1 - num2;
+    var answer = sum;
+    return answer;
+}
+
+
