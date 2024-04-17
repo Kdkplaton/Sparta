bb67611bf9c6df9b3799b10526b4b807c2357a9a
Kdkplaton <kdkplaton@naver.com>
2024-04-17 23:30:25 +0900

    문제 3번 해결

---
+++ Algorithm_CodeKata/No_03.js
@@ -0,0 +1,13 @@
+// 문제 3 - 몫 구하기
+
+// raw code
+function solution(num1, num2) {
+    var answer = 0;
+    return answer;
+}
+
+// 해결 코드
+function solution(num1, num2) {
+    var sum = parseInt(num1 / num2);
+    return sum;
+}
