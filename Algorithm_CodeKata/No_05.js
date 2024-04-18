86a09c04413761ca6efa1a04d86f49a8f65b9458
Kdkplaton <kdkplaton@naver.com>
2024-04-18 09:19:29 +0900

    문제 5번 해결

---
+++ Algorithm_CodeKata/No_05.js
@@ -0,0 +1,19 @@
+// 문제 3 - 몫 구하기
+
+// raw code
+function solution(num1, num2) {
+    var answer = 0;
+    return answer;
+}
+
+// 해결 코드
+function solution_solved(num1, num2) {
+    var res;
+    if(num1 == num2) {
+        res = 1;
+    } else {
+        res = -1;
+    }
+    var answer = res;
+    return answer;
+}
