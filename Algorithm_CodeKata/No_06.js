03dcbd9363c6bcf2894844b40dda7e15e43f3f3c
Kdkplaton <kdkplaton@naver.com>
2024-04-18 09:33:38 +0900

    문제 6번 해결

---
+++ Algorithm_CodeKata/No_06.js
@@ -0,0 +1,14 @@
+// 문제 3 - 몫 구하기
+
+// raw code
+function solution(num1, num2) {
+    var answer = -1;
+    return answer;
+}
+
+// 해결 코드
+function solution_solved(num1, num2) {
+    var res = num1 + num2;
+    var answer = res;
+    return answer;
+}
