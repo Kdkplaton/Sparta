fb340fc76402dd3d53c2be3625cfe60ddf3fa9cf
Kdkplaton <kdkplaton@naver.com>
2024-04-19 09:12:15 +0900

    문제 7번 해결

---
+++ Algorithm_CodeKata/No_07.js
@@ -0,0 +1,14 @@
+// 문제 7 - 두 수의 나눗셈
+
+// 시작 코드
+function solution(num1, num2) {
+    var answer = 0;
+    return answer;
+}
+
+// 해결 코드
+function solution(num1, num2) {
+    var res = parseInt((num1 / num2) * 1000)
+    var answer = res;
+    return answer;
+}
