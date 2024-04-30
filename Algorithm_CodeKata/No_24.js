34778d1d565b1fabfc447c3e212b49b562ceb08f
Kdkplaton <kdkplaton@naver.com>
2024-04-30 10:08:44 +0900

    24번, 25번 풀이 성공

---
+++ Algorithm_CodeKata/No_24.js
@@ -0,0 +1,18 @@
+// 문제 24 - 서울에서 김서방 찾기
+
+// 시작 코드
+function solution(seouls) {
+    var answer = "";
+    return answer;
+}
+
+// 해결 코드
+function solution(seoul) {
+    let str = "";
+    let idx = seoul.indexOf("Kim");
+    
+    str = "김서방은 "+idx+"에 있다";
+    
+    let answer = str;
+    return answer;
+}
---
+++ Algorithm_CodeKata/No_25.js
@@ -0,0 +1,20 @@
+// 문제 25 - 나누어 떨어지는 숫자
+
+// 시작 코드
+function solution(s) {
+    var answer = 0;
+    return answer;
+}
+
+// 해결 코드
+function solution(arr, divisor) {
+    let res = "";
+    res = arr.filter(num => num%divisor==0);
+    if(res == "") res[0] = -1;
+    
+    console.log("정렬 전: "+res);
+    res = res.sort((a, b) => a - b);
+    console.log("정렬 후: "+res);
+    let answer = res;
+    return answer;
+}
