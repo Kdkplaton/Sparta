71b065558d2b304df7ecff4d72b57fe0291980cd
Kdkplaton <kdkplaton@naver.com>
2024-05-03 09:55:04 +0900

    29번 풀이 성공

---
+++ Algorithm_CodeKata/No_29.js
@@ -0,0 +1,19 @@
+// 문제 29 - 제일 작은 수 제거하기
+
+// 시작 코드
+function solution(arr) {
+    var answer = [];
+    return answer;
+}
+
+// 해결 코드
+function solution(arr) {
+    let tar = Number.MAX_SAFE_INTEGER;
+    tar = Math.min(...arr);
+    
+    let answer = [];
+    if (arr.length == 1) { answer = [-1]; }
+    else { answer = arr.filter(value => value!=tar); }
+    
+    return answer;
+}
